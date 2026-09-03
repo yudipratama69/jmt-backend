e70626723bd1282d13e5604446e9a4273a527e3f
yudipratama69 <95265096+yudipratama69@users.noreply.github.com>
2026-09-03 17:53:05 +0700

    Add /member to nitro prerender routes

--- fun-football-admin/nuxt.config.ts
+++ fun-football-admin/nuxt.config.ts
@@ -79,6 +79,7 @@ export default defineNuxtConfig({
         '/verifikasi',
         '/verifikasi-deposit',
         '/admin-keuangan',
+        '/member',
         '/profil-admin',
         '/master-notifikasi'
       ]
